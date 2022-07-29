664450c8e7a3510aa0a55e60a3f6a1fefda5cb34
Elena Titos <eletitos@gmail.com>
2022-07-29 18:01:55 +0200

    Añadidos PropTypes a AddCategory.jsx

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import { useState } from "react";
 
 export const AddCategory = ({onNewCategory}) => {
@@ -26,6 +27,11 @@ export const AddCategory = ({onNewCategory}) => {
         </form>
     );
 }
+
+AddCategory.propTypes = {
+    onNewCategory: PropTypes.func.isRequired,
+}
+
 
 
 
