b90437d8ef74c6828d852d5763b645baeb3ed541
Elena Titos <eletitos@gmail.com>
2022-07-29 20:26:33 +0200

    Añadidos PropTypes a GifGrid

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,5 +1,7 @@
+import PropTypes from "prop-types";
 import useFetchGifs from "../hooks/useFetchGifs";
 import {GifItem} from "./";
+
 
 
 //esto es un componente. 
@@ -24,3 +26,7 @@ export const GifGrid = ( {category} ) => {
     );
 }
 
+
+GifGrid.propTypes = {
+    category: PropTypes.string.isRequired
+}
